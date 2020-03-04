825442c127faeb2b25734b56a2c52fb48872e7e7
Артём Сечко <arsynthdev@gmail.com>
2020-03-04 17:48:58 +0700

    Ignore Pods and .idea files

---
+++ MarvelHeroes/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MarvelHeroes
+//
+//  Created by Артем on 04/03/2020.
+//  Copyright © 2020 Артем. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+
+
+}
